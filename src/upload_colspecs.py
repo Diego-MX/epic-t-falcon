a73db4f3e31a64a25bdef9a9e915af31dd5621a7
Diego Villamil <diego.v@bineo.com>
2022-10-06 17:32:13 -0500

    actualizar feathers

--- src/upload_colspecs.py
+++ src/upload_colspecs.py
@@ -12,20 +12,18 @@ def print_df(a_df, width=180):
 
 
 def fiserv_data(a_df): 
-    fmt_rgx = r'([X9])\((\d+)\)'
-
     mod_cols = {
         'Field Name': lambda df: df['Field Name'].str.strip(), 
         'Format'    : lambda df: df['Format'].astype(str), 
-        'fmt_type'  : lambda df: df['Format'].str.extract(fmt_rgx)[0], 
-        'fmt_len'   : lambda df: df['Format'].str.extract(fmt_rgx)[1], 
+        'fmt_type'  : lambda df: df['Format'].str.extract(r'^([X9])')[0], 
+        'fmt_len'   : lambda df: df['Format'].str.extract(r'.*\((\d+)\)')[0].fillna(1), 
         'aux_date'  : lambda df: df['Technical Mapping'].str.find('YYYYMMDD') > -1, 
         'aux_sign'  : lambda df: df['Technical Mapping'].str.find('+ or -'  ) > -1, 
         'aux_fill'  : lambda df: df['Field Name'].str.match(r'.*filler', case=False), 
-        'name_1'    : lambda df: df['Technical Mapping']
-                .str.strip().str.split(' ').str[0].str.lower().replace(r'\-', '_'), 
+        'name_1'    :(lambda df: df['Technical Mapping']
+                .str.strip().str.split(' ').str[0].str.lower().str.replace('-', '_')), 
         'name'      :(lambda df: np.where(~df['aux_sign'], df['name_1'], 
-                df['name_1'].shift(1) + '_sgn')),
+                df['name_1'].shift(-1) + '_sgn')),
         'chk_len'   :(lambda df: (df['From'] + df['Length'] == df['From'].shift(-1)) 
                 | (np.arange(len(df)) == len(df)-1)), 
         'chk_sign'  :(lambda df: np.where(df['aux_sign'], 
@@ -34,7 +32,8 @@ def fiserv_data(a_df):
 
     b_df = (a_df
         .rename(lambda a_str: a_str.strip(), axis=1)
-        .assign(**mod_cols))
+        .assign(**mod_cols)
+        .astype({'fmt_len': int}))
     return b_df 
     
 
@@ -48,10 +47,10 @@ table_types = ['detail', 'header', 'trailer']
 
 
 
-a_sht, a_tbl, suffix = 'DAMNA001', 'damna', 'detail'
-
-the_cols = ['name', 'From', 'Length', 'Field Name', 'Technical Mapping', 
+the_cols = ['name', 'From', 'Length', 'Format', 'Field Name', 'Technical Mapping', 
         'fmt_type', 'fmt_len', 'aux_date', 'aux_sign', 'aux_fill']
+
+a_sht, a_tbl, suffix = 'DATPTX01', 'atpt', 'detail'
 
 for a_sht, a_tbl in sheets_tables: 
     for suffix in table_types: 
@@ -59,5 +58,5 @@ for a_sht, a_tbl in sheets_tables:
         # PRINT check columns.
 
         table_columns = fiserv_data(pre_table).loc[:, the_cols]
-        table_columns.to_feather (f"refs/catalogs/{a_tbl}_{suffix}.feather") 
+        table_columns.to_feather(f"refs/catalogs/{a_tbl}_{suffix}.feather") 
     
